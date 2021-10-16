58bb7071d38be677c28a0582db6beda83f9a221a
iamzhanaidarmukanov <iamzhanaidar.mukanov@gmail.com>
2021-10-15 20:31:20 -0700

    Created Test File

---
+++ CS3343-Project/src/ProjectTest/ProjectTest1.java
@@ -0,0 +1,14 @@
+package ProjectTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectTest1 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
