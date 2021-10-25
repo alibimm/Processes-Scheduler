017fa50a4d82445acbe4d22be299c3edee8fe10c
nurishok <nurishok.s@gmail.com>
2021-10-25 16:23:59 +0800

    Created algorithm interface class, created algorithm classes with singleton pattern

---
+++ CS3343-Project/src/Project/FB.java
@@ -0,0 +1,18 @@
+package Project;
+
+import java.util.ArrayList;
+
+public class FB implements Algorithm {
+	private static FB instance = new FB();
+	
+	public static FB getInstance() {
+		return instance;
+	}
+	
+	@Override
+	public Result schedule(ArrayList<Process> processes) {
+		// add functionality
+		return null;
+	}
+
+}
---
+++ CS3343-Project/src/Project/SPN.java
@@ -0,0 +1,18 @@
+package Project;
+
+import java.util.ArrayList;
+
+public class SPN implements Algorithm {
+	private static SPN instance = new SPN();
+	
+	public static SPN getInstance() {
+		return instance;
+	}
+
+	@Override
+	public Result schedule(ArrayList<Process> processes) {
+//		Add functionality
+		return null;
+	}
+
+}
