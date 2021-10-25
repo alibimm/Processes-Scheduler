acf842010b45814c76ff79d76b1ef5cd1d8fbd3b
dastanbakirov <dastanbakirov@localhost>
2021-10-25 15:36:29 +0800

    Created Result, ProcessInfo, and ProcessInCPU classes.

---
+++ CS3343-Project/src/Project/ProcessInCPU.java
@@ -0,0 +1,17 @@
+package Project;
+import java.util.*;
+import javafx.util.Pair;
+public class ProcessInCPU {
+	
+	private Process process;
+	private Pair <Integer, Integer> p;
+	
+	public Process getProcess() {
+		return this.process;
+	}
+	public void setProcess(Process p) {
+		this.process = p;
+	}
+	
+	
+}
