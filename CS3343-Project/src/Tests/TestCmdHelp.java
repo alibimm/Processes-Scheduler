65f3a7cae36b796271efc0de179fe2ee88543cd1
iamzhanaidarmukanov <iamzhanaidar.mukanov@gmail.com>
2021-11-25 17:56:35 +0800

    CmdHelp Done

--- CS3343-Project/src/Tests/TestCmdHelp.java
+++ CS3343-Project/src/Tests/TestCmdHelp.java
@@ -28,96 +28,28 @@ class TestCmdHelp {
 		// Running execute new Cmd Help
 		(new CmdHelp()).execute(cmdParts);
 		
-		String expected = "There are 9 commands used in various situations:\n"
+		String expected = "There are 10 commands used in various situations:\n"
 				+ "\n"
-				+ "Working area\n"
-				+ "  readfile [filepathname]		Let the scheduler read the file\n"
-				+ "  schedule				Start the scheduling\n"
-				+ "  suggest				To get suggestions on best algorithm according to currently set indicator (default: Turnaround time) for the given files\n"
-				+ "  custom [indicator]			To get suggestions on best algorithm according to custom indicators for the given files.\n"
-				+ "					Available indicators:\n"
-				+ "					  [AQT] - Average Queueing Time\n"
-				+ "					  [ATRT] - Average TurnAround Time\n"
-				+ "					  [ARTS] - Average Ratio TS (Turnaround/Serviice)\n"
-				+ "					  [CpuUtil] - CPU Utilization\n"
-				+ "  display				Print the current information\n"
-				+ "  help					Get help for execution and navigation\n"
-				+ "  exit					Stop execution of the application\n"
-				+ "\n"
-				+ "\n"
-				+ "To navigate through given files and algorithms for specific results\n"
-				+ "\n"
-				+ "  open [index of file]			Open the detailed results of the specified file\n"
-				+ "  open [algorithm]			Open the detailed results of the specified algorithm\n"
-				+ "  close					Go back to previous information\n"
-				+ "\n"
-				+ "";
-		
-		assertEquals(expected, getOutput());
-	}
-	
-	@Test
-	void testHelp2() throws Exception  {
-		
-		setOutput();
-		
-		String[] cmdParts = new String[]{"help"};
-		
-		// Running execute new Cmd Help
-		(new CmdHelp()).execute(cmdParts);
-		
-		cmdParts = new String[]{"help"};
-		
-		// Running execute new Cmd Help
-		(new CmdHelp()).execute(cmdParts);
-		
-		String expected = "There are 9 commands used in various situations:\n"
-				+ "\n"
-				+ "Working area\n"
-				+ "  readfile [filepathname]		Let the scheduler read the file\n"
-				+ "  schedule				Start the scheduling\n"
-				+ "  suggest				To get suggestions on best algorithm according to currently set indicator (default: Turnaround time) for the given files\n"
-				+ "  custom [indicator]			To get suggestions on best algorithm according to custom indicators for the given files.\n"
-				+ "					Available indicators:\n"
-				+ "					  [AQT] - Average Queueing Time\n"
-				+ "					  [ATRT] - Average TurnAround Time\n"
-				+ "					  [ARTS] - Average Ratio TS (Turnaround/Serviice)\n"
-				+ "					  [CpuUtil] - CPU Utilization\n"
-				+ "  display				Print the current information\n"
-				+ "  help					Get help for execution and navigation\n"
-				+ "  exit					Stop execution of the application\n"
-				+ "\n"
+				+ "  readfile [filepathname]  Let the scheduler read the file                   \n"
+				+ "  schedule                 Start the scheduling                              \n"
+				+ "  suggest                  To get suggestions on best algorithm according to currently set indicator for the given files.\n"
+				+ "				(default: Turnaround time)\n"
+				+ "  custom [indicator]       To get suggestions on best algorithm according to custom indicators for the given files.\n"
+				+ "		  	    Available indicators:\n"
+				+ "				[AQT] - Average Queueing Time\n"
+				+ "				[ATRT] - Average TurnAround Time\n"
+				+ "				[ARTS] - Average Ratio TS (Turnaround/Service)\n"
+				+ "				[CpuUtil] - CPU Utilization\n"
+				+ "  display                  Print the current information                     \n"
+				+ "  help                     Get help for execution and navigation             \n"
+				+ "  exit                     Stop execution of the application\n"
+				+ "                \n"
 				+ "\n"
 				+ "To navigate through given files and algorithms for specific results\n"
 				+ "\n"
-				+ "  open [index of file]			Open the detailed results of the specified file\n"
-				+ "  open [algorithm]			Open the detailed results of the specified algorithm\n"
-				+ "  close					Go back to previous information\n"
-				+ "\n"
-				+ "There are 9 commands used in various situations:\n"
-				+ "\n"
-				+ "Working area\n"
-				+ "  readfile [filepathname]		Let the scheduler read the file\n"
-				+ "  schedule				Start the scheduling\n"
-				+ "  suggest				To get suggestions on best algorithm according to currently set indicator (default: Turnaround time) for the given files\n"
-				+ "  custom [indicator]			To get suggestions on best algorithm according to custom indicators for the given files.\n"
-				+ "					Available indicators:\n"
-				+ "					  [AQT] - Average Queueing Time\n"
-				+ "					  [ATRT] - Average TurnAround Time\n"
-				+ "					  [ARTS] - Average Ratio TS (Turnaround/Serviice)\n"
-				+ "					  [CpuUtil] - CPU Utilization\n"
-				+ "  display				Print the current information\n"
-				+ "  help					Get help for execution and navigation\n"
-				+ "  exit					Stop execution of the application\n"
-				+ "\n"
-				+ "\n"
-				+ "To navigate through given files and algorithms for specific results\n"
-				+ "\n"
-				+ "  open [index of file]			Open the detailed results of the specified file\n"
-				+ "  open [algorithm]			Open the detailed results of the specified algorithm\n"
-				+ "  close					Go back to previous information\n"
-				+ "\n"
-				+ "";
+				+ "  open [index of case]     Open the detailed results of the specified file   \n"
+				+ "  open [algorithm]         Open the detailed results of the specified algorithm\n"
+				+ "  close                    Go back to previous information                   \n\n";
 		
 		assertEquals(expected, getOutput());
 	}
