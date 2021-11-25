52a0a9b960c69216caff03c7daca0ca4b25cae57
iamzhanaidarmukanov <iamzhanaidar.mukanov@gmail.com>
2021-11-25 10:12:12 +0800

    Added testMain and changed testCmdReadFile

---
+++ CS3343-Project/src/Tests/TestMain.java
@@ -0,0 +1,164 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.Test;
+
+import Project.Main;
+
+class TestMain {
+	
+	Main wws = new Main();
+
+	@Test
+	void testReadFile() throws Exception {
+		
+		setOutput();
+		
+		String expected = "";
+		ByteArrayInputStream in = new ByteArrayInputStream("readfile ./src/TestSamples/1-perfect.txt".getBytes());
+		System.setIn(in);
+		Main.main(null);
+		String actual = getOutput();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testSchedule() throws Exception {
+		
+		setOutput();
+		
+		String expected = "";
+		ByteArrayInputStream in = new ByteArrayInputStream("schedule".getBytes());
+		System.setIn(in);
+		Main.main(null);
+		String actual = getOutput();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testCustom() throws Exception {
+		
+		setOutput();
+		
+		String expected = "";
+		ByteArrayInputStream in = new ByteArrayInputStream("custom AQT".getBytes());
+		System.setIn(in);
+		Main.main(null);
+		String actual = getOutput();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testSuggest() throws Exception {
+		
+		setOutput();
+		
+		String expected = "";
+		ByteArrayInputStream in = new ByteArrayInputStream("suggest".getBytes());
+		System.setIn(in);
+		Main.main(null);
+		String actual = getOutput();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testOpen() throws Exception {
+		
+		setOutput();
+		
+		String expected = "";
+		ByteArrayInputStream in = new ByteArrayInputStream("open FCFS".getBytes());
+		System.setIn(in);
+		Main.main(null);
+		String actual = getOutput();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testClose() throws Exception {
+		
+		setOutput();
+		
+		String expected = "";
+		ByteArrayInputStream in = new ByteArrayInputStream("close".getBytes());
+		System.setIn(in);
+		Main.main(null);
+		String actual = getOutput();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testDisplay() throws Exception {
+		
+		setOutput();
+		
+		String expected = "";
+		ByteArrayInputStream in = new ByteArrayInputStream("display".getBytes());
+		System.setIn(in);
+		Main.main(null);
+		String actual = getOutput();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testHelp() throws Exception {
+		
+		setOutput();
+		
+		String expected = "";
+		ByteArrayInputStream in = new ByteArrayInputStream("help".getBytes());
+		System.setIn(in);
+		Main.main(null);
+		String actual = getOutput();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testZExit() throws Exception {
+		
+		setOutput();
+		
+		String expected = "";
+		ByteArrayInputStream in = new ByteArrayInputStream("exit".getBytes());
+		System.setIn(in);
+		Main.main(null);
+		String actual = getOutput();
+		System.out.println("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQ" + actual + "QQQQQQQQQQQQQQQQQQQQQQ");
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testNonExisting() throws Exception {
+		
+		setOutput();
+		
+		String expected = "";
+		ByteArrayInputStream in = new ByteArrayInputStream("NonExisting".getBytes());
+		System.setIn(in);
+		Main.main(null);
+		String actual = getOutput();
+		assertEquals(expected, actual);
+	}
+	
+	// Handling output
+	PrintStream oldPrintStream;
+	ByteArrayOutputStream bos;
+
+	  private void setOutput() throws Exception {
+	    oldPrintStream = System.out;
+	    bos = new ByteArrayOutputStream();
+	    System.setOut(new PrintStream(bos));
+	  }
+
+	  private String getOutput() { // throws Exception
+	    System.setOut(oldPrintStream);
+	    return bos.toString();
+	  }
+	
+
+}
