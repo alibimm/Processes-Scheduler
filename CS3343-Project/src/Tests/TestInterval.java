12f71dbbc83c485152758b1c4cd0872c4e9d2fd9
iamzhanaidarmukanov <iamzhanaidar.mukanov@gmail.com>
2021-12-03 21:16:57 +0800

    Clean TestInterval

--- CS3343-Project/src/Tests/TestInterval.java
+++ CS3343-Project/src/Tests/TestInterval.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.Test;
 import Project.Interval;
 
 public class TestInterval {
+	
+	//Initialize testing instance
+	Interval testInterval = Interval.create(0,5);
+			
+			
 	@Test
 	// Testing create() method in Interval class
 	void TestCreate() {
-		//Initialize testing instance
-		Interval testInterval = Interval.create(0,5);
-		
 		assertEquals(testInterval.getStart(), 0);
 		assertEquals(testInterval.getEnd(), 5);
 	}
@@ -20,18 +22,12 @@ public class TestInterval {
 	@Test
 	// Testing getStart() method in Interval class
 	void TestGetStart() {
-		//Initialize testing instance
-		Interval testInterval = Interval.create(0,5);
-		
 		assertEquals(testInterval.getStart(), 0);
 	}
 	
 	@Test
 	// Testing getEnd() method in Interval class
 	void TestGetEnd() {
-		//Initialize testing instance
-		Interval testInterval = Interval.create(0,5);
-		
 		assertEquals(testInterval.getEnd(), 5);
 	}
 }
