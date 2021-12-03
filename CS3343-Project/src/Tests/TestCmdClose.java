7eabe94b32aaf6fb42f7af06a4cb7de7b3bcfa7a
iamzhanaidarmukanov <iamzhanaidar.mukanov@gmail.com>
2021-12-04 03:15:22 +0800

    Fixed and Cleaned TestCmdClose

--- CS3343-Project/src/Tests/TestCmdClose.java
+++ CS3343-Project/src/Tests/TestCmdClose.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import Commands.CmdClose;
+import Commands.CmdDisplay;
 import Commands.CmdReadFile;
 import Project.Case;
 import Project.MainSystem;
@@ -20,23 +21,13 @@ class TestCmdClose {
 
 	ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tear() {
-	}
-
 	@BeforeEach
 	void setUp() {
+		MainSystem system = MainSystem.getInstance();
+		system.clear();
 	    System.setOut(new PrintStream(outContent));
 	}
 
-	@AfterEach
-	void tearDown() {
-	}
-	
 	@Test
 	void testCmdCloseInTheRoot() {
 		
@@ -67,40 +58,7 @@ class TestCmdClose {
 	}
 	
 	@Test
-	void testCmdCloseInTheRoot2() {
-		
-		// Getting instance of the Main System
-		MainSystem system = MainSystem.getInstance();
-		
-		// Initializing cmdParts that will be inputted
-		String[] cmdParts = new String[]{"readfile", "./src/TestSamples/1-perfect.txt"};
-		
-		// Running execute new Cmd Display
-		(new CmdReadFile()).execute(cmdParts);
-		
-		cmdParts = new String[] {"close"};
-		
-		// Running execute new Cmd close
-		(new CmdClose()).execute(cmdParts);
-		
-		boolean expected = false;
-		
-		String expected_str = "You are already in the root directory";
-		
-		boolean actual = system.close();
-		
-		assertEquals(expected, actual);
-		
-		assertEquals(expected_str, outContent.toString().trim());
-		
-	}
-	
-	@Test
-	void testCmdCloseInAlgo() {
-		
-
-		// Getting instance of the Main System
-		MainSystem system = MainSystem.getInstance();
+	void testCmdCloseInAlgo() throws Exception {
 		
 		// Initializing cmdParts that will be inputted
 		String[] cmdParts = new String[]{"readfile", "./src/TestSamples/1-perfect.txt"};
@@ -116,14 +74,50 @@ class TestCmdClose {
 		cmdParts = new String[]{"open", "FCFS"};
 		(new CmdClose()).execute(cmdParts);
 		
-		// Running open command
+		// Running close command
 		cmdParts = new String[]{"close"};
 		(new CmdClose()).execute(cmdParts);
 		
-		boolean expected = true; 
+		String expected = "You are already in the root directory"
+				+ "\nYou are already in the root directory"
+				+ "\nYou are already in the root directory";
 		
-		assertEquals(expected, true);
+		String actual = outContent.toString().trim();
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	void testCmdCloseInCase() throws Exception {
+		
+		// Initializing cmdParts that will be inputted
+		String[] cmdParts = new String[]{"readfile", "./src/TestSamples/1-perfect.txt"};
+		
+		// Running execute new Cmd Display
+		(new CmdReadFile()).execute(cmdParts);
+		
+		// Running schedule command
+		cmdParts = new String[]{"schedule"};
+		(new CmdClose()).execute(cmdParts);
+		
+		// Running open command
+		cmdParts = new String[]{"open", "0"};
+		(new CmdClose()).execute(cmdParts);
+		
+		// Running close command
+		cmdParts = new String[]{"close"};
+		(new CmdClose()).execute(cmdParts);
+		
+		String expected = "You are already in the root directory"
+				+ "\nYou are already in the root directory"
+				+ "\nYou are already in the root directory";
+		
+		String actual = outContent.toString().trim();
+		
+		assertEquals(expected, actual);
 		
 	}
 
+	
 }
